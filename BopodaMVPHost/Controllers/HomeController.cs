90c7d267bfeb072e73f381f5caba97416eebd893
Anduin2017 <anduin@aiursoft.com>
2019-10-18 14:50:35 +0800

    Add home controller

---
+++ BopodaMVPHost/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BopodaMVPHost.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Json(new
+            {
+                Message = "Welcome to bopoda MVP API Server",
+                User.Identity.IsAuthenticated
+            });
+        }
+    }
+}
